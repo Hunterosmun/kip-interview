30fcabb2ea2c144e9008c89ed42e5c7f8f2ed2a7
Hunter Osmun <Hunter@Osmun.net>
2022-02-10 09:28:45 -0700

    Golf, shrunk by 11 lines

--- index.js
+++ index.js
@@ -4,33 +4,23 @@ const assert = require('assert')
 
 const example = require('./input.json')
 
-function parse (item, toFind = 'steps', propertyName) {
-  const names = []
-  if (Array.isArray(item)) {
-    names.push(
-      ...item.flatMap(key => {
-        return parse(item[key], toFind, propertyName)
-      })
-    )
-  }
+function parse (item, toFind = 'steps', propName = 'name') {
+  let found = []
+  if (Array.isArray(item)) item.flatMap(k => [...parse(k, toFind, propName)])
   if (typeof item === 'object') {
-    names.push(
-      ...Object.keys(item).flatMap(key => {
-        if (key === toFind)
-          names.push(...gatherElements(item[key], propertyName))
-        if (typeof item[key] === 'object' || Array.isArray(item[key])) {
-          return parse(item[key], toFind, propertyName)
-        }
-      })
-    )
+    for (let key in item) {
+      if (key === toFind) found.push(...gatherElements(item[key], propName))
+      if (typeof item[key] === 'object' || Array.isArray(item[key])) {
+        found.push(...parse(item[key], toFind, propName))
+      }
+    }
   }
-  return [...names].filter(Boolean)
+  return [...found]
 }
 
-function gatherElements (arr, propertyName) {
-  return arr.flatMap(el => {
-    return el[propertyName]
-  })
+function gatherElements (arr, propName) {
+  return arr.flatMap(el => el[propName])
 }
 
+console.log(parse(example, 'steps', 'name'))
 console.log(parse(example, 'steps', 'type'))
